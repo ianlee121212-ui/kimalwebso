31fcf6c4cf66f791a1ca0eb1925cd19e2bf05fc5
ianlee121212-ui <ianlee121212@gmail.com>
2026-06-16 13:17:03 +0900

    Update app.py

--- front/app.py
+++ front/app.py
@@ -6,15 +6,15 @@ BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 st.set_page_config(page_title="Websosal plannerr", layout="centered")
 
 st.title("장르 및 플롯 추천 시스템")
-st.write("당신의 집필 성향과 선호하는 키워드를 기반으로 추천합니다.")
+st.write("집필 성향과 선호 키워드 기반으로 추천합니다.")
 st.markdown("***")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    target_audience = st.radio("독자층 선택", ["남성향", "여성향"])
+    target_audience = st.radio("타겟층 선택", ["남성향", "여성향"])
     main_trope = st.selectbox(
-        "핵심 웹소설 트로프(Trope)", 
+        "웹소설 뼈대 소재", 
         ["회빙환(회귀/빙의/환생)", "전문가", "성장물"]
     )
 
@@ -23,7 +23,7 @@ with col2:
         "선호하는 분위기",
         options=["가벼움", "무거움", "피폐"]
     )
-    writing_speed = st.slider("연재 분량 (편)", 1, 7, 3)
+    writing_speed = st.slider("연재 분량 편", 1, 7, 3)
 
 if st.button("연재 가이드"):
     payload = {
@@ -51,9 +51,9 @@ if st.button("연재 가이드"):
                 st.subheader("전략")
                 st.warning(result["recommendedstra"])
                 
-                st.metric(label="요즘 유행하는 웹소설 키워드", value=result["keyword"])
+                st.metric(label="요즘 유행하는 웹소설 키워드 랜덤", value=result["keyword"])
             else:
-                st.error(f"백엔드 서버 오류가 발생했습니다. (코드: {response.status_code})")
+                st.error(f"서버 오류가 발생했습니다. (코드: {response.status_code})")
                 
     except requests.exceptions.ConnectionError:
-        st.error("⚠️ FastAPI 백엔드 서버에 연결할 수 없습니다. Docker 컨테이너 상태를 확인하세요.")
+        st.error("서버에 연결할 수 없습니다.")
